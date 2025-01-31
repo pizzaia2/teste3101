4d1d4486319ae1f1582b0ea597d1c68667907111
pizzaia2 <cc9876811@gmail.com>
2025-01-31 01:01:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/seu-repositorio/',  // Adicione esta linha para especificar a base do GitHub Pages
   server: {
     host: "::",
     port: 8080,
