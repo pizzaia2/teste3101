46fc39f5cc775054581ff2c37836b3c96e47cc05
pizzaia2 <cc9876811@gmail.com>
2025-01-31 01:27:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/teste3101/',  // Adicionando a base correta para o GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
+  base: "/nome-do-repositorio/", // Adicione o nome do repositório aqui
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // Pasta de saída do build
+    emptyOutDir: true, // Limpa a pasta de saída antes de cada build
   },
 }));
